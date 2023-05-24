ad6e339c70b1170550ab683437ed947bc1d75d18
Kairaa <118424778+Tech-Kairaa@users.noreply.github.com>
2023-05-24 11:10:41 +0530

    test

--- pages/account/logout.js
+++ pages/account/logout.js
@@ -1,14 +1,12 @@
-import { updateAccess } from '@/services/auth.utils';
 import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
-import { useDispatch } from 'react-redux';
 
 const Logout = () => {
 	const router = useRouter();
-	// const to = router.query.to;
-	const dispatch = useDispatch();
+	// // const to = router.query.to;
+	// const dispatch = useDispatch();
 	Cookies.remove('token');
-	dispatch(updateAccess(false));
+	// dispatch(updateAccess(false));
 	router.push('/');
 };
 
